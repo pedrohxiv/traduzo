cf4e2aaf64bc8fbc4f88f16064e5645754852f55
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2023-08-25 16:50:18 -0300

    Requisito 09

--- tests/controllers/admin/test_admin_controller.py
+++ tests/controllers/admin/test_admin_controller.py
@@ -1,6 +1,25 @@
-# from src.models.history_model import HistoryModel
-# from src.models.user_model import UserModel
+from src.models.history_model import HistoryModel
+from src.models.user_model import UserModel
 
 
 def test_history_delete(app_test):
-    raise NotImplementedError
+    user = UserModel({
+        "name": "admin",
+        "level": "admin",
+        "token": "token",
+    })
+    user.save()
+
+    history = HistoryModel({
+        "text-to-translate": "House",
+        "translate-from": "en",
+        "translate-to": "pt",
+    })
+    history.save()
+
+    response = app_test.delete(f"/admin/history/{history.id}", headers={
+      "Authorization": "token",
+      "User": "admin",
+    })
+
+    assert response.status_code == 204
